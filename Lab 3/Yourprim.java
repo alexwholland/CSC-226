af62b3e564b4ca52b9186ce0daf1164e582e0a06
Alex Holland <64433812+alexwholland@users.noreply.github.com>
2022-05-02 18:40:51 -0700

    Lab 3 complete

---
+++ Lab 3/Yourprim.java
@@ -0,0 +1,128 @@
+import edu.princeton.cs.algs4.*;
+
+// We implement Prim's algorithm using data structures implemented in edu.princeton.cs.algs4.
+// Download link: https://algs4.cs.princeton.edu/code/algs4.jar
+// Documentation: https://algs4.cs.princeton.edu/code/
+//
+// Specifically, we will use EdgeWeightedGraph, Edge, MinPQ, and Queue classes.
+
+public class Yourprim {
+    public static void main(String[] args) {
+        // A) copy and modify the main function in test.java but change the data
+        // structure to a weightedgraph
+        In in = new In(args[0]);
+        EdgeWeightedGraph G = new EdgeWeightedGraph(in);
+        boolean[] marked = new boolean[G.V()]; // In the early Monday tutorial, Boolean is a class whereas boolean is a
+        Queue<Edge> mst = new Queue<Edge>(); // A linked list could also work.
+        marked[0] = true;
+
+        // Data structures and initializations associated with the maximum spanning tree
+        boolean[] markedM = new boolean[G.V()];
+        Queue<Edge> mstM = new Queue<Edge>();
+        markedM[0] = true;
+
+        while (true) {
+            // In order to find a minimum crossing edge, we loop through all the edges in
+            // the graph and add all of the crossing edges to a minimum priority queue.
+            MinPQ<Edge> minpq = new MinPQ<Edge>();
+
+            for (Edge e : G.edges()) {
+                int u = e.either();
+                int v = e.other(u);
+                // B) Check the condition if they are cross edges and insert to pq
+                if ((marked[u] && !marked[v]) || !marked[u] && marked[v]) {
+                    minpq.insert(e);
+                }
+            }
+
+            // If the priority queue is empty, then this means there are no more crossing
+            // edges so we are done.
+            if (minpq.isEmpty())
+                break;
+
+            // If the priority queue is not empty, then the minimum element in is a
+            // minimum crossing edge.
+
+            // C) get the edge minimum from pq
+            Edge minEdge = minpq.delMin();
+
+            // D) Update the (current) MST and Mark the other vertex
+            if (G != null) {
+                int u = minEdge.either();
+                if (marked[u] == false) mst.enqueue(minEdge);
+                for (Edge e : G.adj(u)) {
+                    if (marked[e.other(u)] == false) minpq.insert(e);
+                }
+                marked[u] = true;
+
+                int v = minEdge.other(u);
+                if (marked[v] == false) mst.enqueue(minEdge);
+                for (Edge e : G.adj(v)) {
+                    if (marked[e.other(v)] == false) minpq.insert(e);
+                }
+                marked[v] = true;
+            }
+        }
+        //Adapted implementation for Maximum MST (template the same as above)
+        while (true) {
+            // In order to find a maximum crossing edge, we loop through all the edges in
+            // the graph and add all of the crossing edges to a maximum priority queue.
+            MaxPQ<Edge> maxpq = new MaxPQ<Edge>();
+
+            for (Edge e : G.edges()) {
+                int u = e.either();
+                int v = e.other(u);
+                // B) Check the condition if they are cross edges and insert to pq
+                if ((!markedM[u] && markedM[v]) || (markedM[u] && !markedM[v])) {
+                    maxpq.insert(e);
+                }
+            }
+
+            // If the priority queue is empty, then this means there are no more crossing
+            // edges so we are done.
+            if (maxpq.isEmpty())
+                break;
+
+            // If the priority queue is not empty, then the maximum element in it is a
+            // maximum crossing edge.
+
+            // C) get the edge maximum from pq
+            Edge maxEdge = maxpq.delMax();
+
+            // D) Update the (current) MST and Mark the other vertex
+            if (G != null) {
+                int u = maxEdge.either();
+                if (markedM[u] == false) mstM.enqueue(maxEdge);
+                for (Edge e : G.adj(u)) {
+                    if (markedM[e.other(u)] == false) maxpq.insert(e);
+                }
+                markedM[u] = true;
+
+                int v = maxEdge.other(u);
+                if (markedM[v] == false) mstM.enqueue(maxEdge);
+                for (Edge e : G.adj(v)) {
+                    if (markedM[e.other(v)] == false) maxpq.insert(e);
+                }
+                markedM[v] = true;
+            }
+        }
+
+        // Once a Minimum MST has been found, print its edges and total weight.
+        System.out.println("Minimum Spanning Tree:");
+        printWeight(mst);
+        // Once a Maximum MST has been found, print its edges and total weight.
+        System.out.println("\nMaximum Spanning Tree:");
+        printWeight(mstM);
+    }
+
+    private static void printWeight(Queue<Edge> mst) {
+        double eWeight = 0.0;
+        for (Edge e : mst) {
+            StdOut.println(e);
+            eWeight += e.weight();
+        }
+        // E) Evaluate the weight of MST (minimum)
+        StdOut.printf("Total weight is=%.5f\n", eWeight);
+    }
+
+}
